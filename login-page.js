bc003c83c05cbd22e122c7866f8d5cbb08d29dfa
Ankita <gankita009@gmail.com>
2023-02-22 15:59:29 +0530

    firebase

--- login-page.js
+++ login-page.js
@@ -4,14 +4,57 @@
 /*var webengage;!function(w,e,b,n,g){function o(e,t){e[t[t.length-1]]=function(){r.__queue.push([t.join("."),
 arguments])}}var i,s,r=w[b],z=" ",l="init options track screen onReady".split(z),a="feedback survey notification".split(z),c="options render clear abort".split(z),p="Open Close Submit Complete View Click".split(z),u="identify login logout setAttribute".split(z);if(!r||!r.__v){for(w[b]=r={__queue:[],__v:"6.0",user:{}},i=0;i < l.length;i++)o(r,[l[i]]);for(i=0;i < a.length;i++){for(r[a[i]]={},s=0;s < c.length;s++)o(r[a[i]],[a[i],c[s]]);for(s=0;s < p.length;s++)o(r[a[i]],[a[i],"on"+p[s]])}for(i=0;i < u.length;i++)o(r.user,["user",u[i]]);setTimeout(function(){var f=e.createElement("script"),d=e.getElementById("_webengage_script_tag");f.type="text/javascript",f.async=!0,f.src=("https:"==e.location.protocol?"https://ssl.widgets.webengage.com":"http://cdn.widgets.webengage.com")+"/js/webengage-min-v-6.0.js",d.parentNode.insertBefore(f,d)})}}(window,document,"webengage");webengage.init("311c522c");
 */
+
+//firebase
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+   // import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-analytics.js";
+    import { getAuth } from "firebase/auth";
+    import { getDatabase } from "firebase/database";
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyCR_zbbjBRYGeA4yXfLU7QyJe3YPL-shCU",
+      authDomain: "ankita-androidios.firebaseapp.com",
+      databaseURL: "https://ankita-androidios-default-rtdb.firebaseio.com",
+      projectId: "ankita-androidios",
+      storageBucket: "ankita-androidios.appspot.com",
+      messagingSenderId: "250676655438",
+      appId: "1:250676655438:web:d66f3372bd929b74c0da1f",
+      measurementId: "G-1LSW5Z7W9B"
+    };
   
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+   // const analytics = getAnalytics(app);
+    const auth = getAuth(app);
+    const database = getDatabase(app);
+
+
 //login function
     const loginForm = document.getElementById("login-form");
     const loginErrorMsg = document.getElementById("login-error-msg");
     function signIn(){
       const username = loginForm.username.value;
       const password = loginForm.password.value;
-      if ((username === "ankita" && password === "123") || (username=== "sid" && password === "123") || (username==="lakshya" && password === "123") || (username==="neha" && password === "123") || (username === "mishti" && password === "123") || (username === "anku" && password === "123") || (username === "Pratap" && password === "123")) {
+      
+      if (username == false || password == false){
+        loginErrorMsg.style.opacity = 1;
+      }
+      //firebase login
+      auth.signInWithEmailAndPassword().then(function(username, password){
+        const user = auth.currentUser;
+        const uid = user.uid;
+        webengage.user.login(uid);
+        alert("You have successfully logged in.");
+        window.location.href="signin.html"
+      }).catch(function(error){
+        var error_code = error.code;
+        var error_message = error.message;
+        alert(error_message)
+      })
+    }
+
+
+    /*  if ((username === "ankita" && password === "123") || (username=== "sid" && password === "123") || (username==="lakshya" && password === "123") || (username==="neha" && password === "123") || (username === "mishti" && password === "123") || (username === "anku" && password === "123") || (username === "Pratap" && password === "123")) {
         webengage.user.login(username);
         if(username == "ankita"){
           webengage.user.setAttribute('we_email', 'gankita009@gmail.com');
@@ -82,7 +125,7 @@ else{
         else {
           loginErrorMsg.style.opacity = 1;
         }
-      }
+      }*/
 
 //feedback function
 function feedback(){
